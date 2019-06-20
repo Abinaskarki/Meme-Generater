779e25e002b8cc2c2263d6b52d6480cfc0b14764
AbinasKarki <abinaskarki14@gmail.com>
2019-06-20 09:19:51 -0400

    First Commit

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+import styles from "./styles.css"
+
+function Header(){
+	return(
+		<header>
+			<img 
+			src = "http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+			alt = "Problem?" />
+		<h1>Meme Generator</h1>
+		</header>
+	)
+}
+
+export default Header
